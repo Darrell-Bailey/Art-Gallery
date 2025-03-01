dcb67e837cc973d0194ff32c882c7a381099cef1
Darrell Bailey <darrelljsbailey@gmail.com>
2025-03-01 11:25:15 -0500

    Added navigation to UI infrastructure

--- app/src/main/java/com/example/artgallery/MainActivity.kt
+++ app/src/main/java/com/example/artgallery/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.artgallery
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,8 +14,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -23,8 +26,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artgallery.ui.theme.ArtGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -72,6 +78,9 @@ fun ArtGallery(modifier: Modifier = Modifier) {
         }
         Text(
             text = "City Landscapes",
+            textAlign = TextAlign.Center,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
             modifier = modifier
                 .padding(16.dp)
                 .fillMaxHeight(0.2F)
@@ -79,16 +88,23 @@ fun ArtGallery(modifier: Modifier = Modifier) {
         )
         Row (
             modifier = modifier
-                .fillMaxHeight(0.2F)
+                .fillMaxHeight(0.2F),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ){
-
+            NavigateGallery({}, "Previous")
+            NavigateGallery({}, "Next")
         }
     }
 }
 
 @Composable
-fun navigateGallery(){
-
+fun NavigateGallery(onClick: () -> Unit, descriptor: String){
+    Button (
+        onClick = onClick
+    ) {
+        Text(text = descriptor)
+    }
 }
 
 @Preview(showBackground = true)
